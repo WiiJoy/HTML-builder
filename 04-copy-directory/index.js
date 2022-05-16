7b5134676cbc4c149ac2848680ecde62571eeb53
WiiJoy MacOS <wiijoy@bk.ru>
2022-05-16 07:59:29 +0300

    feat: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.rm(path.join(__dirname, 'files-copy'), {force: true}, () => {
+    fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, () => {
+        fs.readdir(path.join(__dirname, 'files'), {withFileTypes: true}, (err, res) => {
+            if (err) {
+                console.log(err)
+            }
+    
+            res.forEach(item => {
+                if (item.isFile()) {
+                    fs.copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'files-copy', item.name), () => {})
+                }
+            })
+        })
+    })
+
+    
+})
+
 
