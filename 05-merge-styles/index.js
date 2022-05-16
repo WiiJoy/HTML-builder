466515fc7094c03507745002a4ed2b3ccc90a8b4
WiiJoy MacOS <wiijoy@bk.ru>
2022-05-16 14:05:59 +0300

    refactor: cleanup -5-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const styles = []
 const writeBundle = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'))
 
 fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err, res) => {
@@ -10,20 +9,11 @@ fs.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}, (err, res) => 
     }
 
     res.forEach(item => {
-        console.log(item.name)
-        // if (item.isFile()) {
-        //     fs.copyFile(path.join(__dirname, 'files', item.name), path.join(__dirname, 'files-copy', item.name), () => {})
-        // }
 
         if (item.isFile() && path.extname(item.name) === '.css') {
             fs.readFile(path.join(__dirname, `styles`, item.name), (err, data) => {
                 writeBundle.write(data)
-                // console.log(data.toString())
-                // styles.push(data.toString())
-                // console.log(styles)
             })
         }
     })
-
-    // console.log(styles)
 })
